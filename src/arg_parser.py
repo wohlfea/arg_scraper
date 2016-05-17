6d9bddfd45a5fe474ee7c92bd46c7fcd6e2c62b8
wohlfea <wohlfea@debian.home>
2016-05-16 22:09:44 -0700

    now works when searching for class names.

--- src/arg_parser.py
+++ src/arg_parser.py
@@ -1,13 +1,14 @@
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 from sys import argv
+from pprint import pprint
 import urllib
 
 
 def main(url, target):
     r = urllib.urlopen(url).read()
     soup = BeautifulSoup(r)
-    print(soup.prettify())
-
+    words = soup.find_all(class_=str(target))
+    pprint(words)
 
 if __name__ == '__main__':
     main(argv[1], argv[2])
